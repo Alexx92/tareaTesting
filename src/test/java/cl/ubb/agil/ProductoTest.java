46d14936fcfac200878616efa369c6899981b5f5
Hernan <alexx92.04@gmail.com>
2016-04-21 00:01:26 -0300

    Commit hr 00

--- src/test/java/cl/ubb/agil/ProductoTest.java
+++ src/test/java/cl/ubb/agil/ProductoTest.java
@@ -1,5 +1,21 @@
 package cl.ubb.agil;
 
+import org.junit.Test;
+
 public class ProductoTest {
+
+	
+	@Test
+	public void creaProductoConCategoria(){
+		Categoria categoria=new Categoria("Guitarra Electrica","1578");
+		Producto producto = new Producto("Guitarra Gibson Les Paul Studio",5,10,categoria);
+		
+	}
+	@Test
+	public void creaProductoSinCategoria(){
+		
+		Producto producto = new Producto("Guitarra Gibson Les Paul Studio",5,10);
+		
+	}
 
 }
